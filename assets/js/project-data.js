5886e09d626bdf7cacc6183b275b9015d0f9587a
Naimah Damayanti <naimahdamayanti7@gmail.com>
2025-05-30 12:34:17 +0700

    first commit

---
+++ assets/js/project-data.js
@@ -0,0 +1,54 @@
+const project = {
+  "academic-system": {
+    title: "Academic System",
+    category: "Web",
+    date: "14 Mei 2025",
+    url: "https://github.com/naimahdamayanti/Naimah_eval_pbf_Tugas1",
+    urlText: "www.academicsystem.com",
+    description: "Academic System adalah sebuah website yang digunakan untuk memudahkan dalam mengelola data mahasiswa dan dosen. Website ini memungkinkan pengguna untuk mengelola data mahasiswa dan dosen dengan mudah dan cepat. Project ini saya buat pada semester 4 di Politeknik Negeri Cilacap.",
+    images: [
+      "assets/img/portfolio/dashboard-1.png",
+      "assets/img/portfolio/dosen.png",
+      "assets/img/portfolio/tambah-dosen.png",
+      "assets/img/portfolio/edit-mahasiswa.png"
+    ]
+  },
+  "monitoring-kehadiran": {
+    title: "Monitoring Kehadiran",
+    category: "Web",
+    date: "11 Maret 2025",
+    url: "https://github.com/naimahdamayanti/FE-Kehadiran",
+    urlText: "www.monitoringkehadiran.com",
+    description: "Sistem monitoring kehadiran adalah sebuah website yang digunakan untuk memudahkan dalam mengelola data kehadiran mahasiswa untuk dosen dan admin. Pada project ini saya berperan menjadi Front-End Developer. Project ini saya buat pada semester 4 di Politeknik Negeri Cilacap.",
+    images: [
+      "assets/img/portfolio/dashboard-2.png",
+      "assets/img/portfolio/hal-dosen.png",
+      "assets/img/portfolio/tambah-dosen2.png",
+      "assets/img/portfolio/edit-dosen2.png"
+    ]
+  },
+  "dreamcorp": {
+    title: "DREAMCORP",
+    category: "Web",
+    date: "03 April 2024",
+    url: "https://github.com/naimahdamayanti/Pengelolaan-Administrasi",
+    urlText: "www.dreamcorp.com",
+    description: "DREAMCORP adalah sebuah website manajemen data pegawai yang dirancang untuk mempermudah pengelolaan informasi karyawan secara efisien dan terstruktur. Website ini dikembangkan pada semester 2 di Politeknik Negeri Cilacap sebagai bagian dari proyek pembelajaran, dengan fitur-fitur seperti pencatatan data pegawai, pengelompokan berdasarkan jabatan, dan pengelolaan informasi secara digital.",   
+    images: [
+      "assets/img/portfolio/dreamcorp.png"
+    ]
+  },
+  "data-guru": {
+    title: "Data Guru",
+    category: "Web",
+    date: "19 Juni 2024",
+    url: "https://github.com/naimahdamayanti/PWEB2/tree/main/db_guru",
+    urlText: "www.dataguru.com",
+    description: "Sistem manajemen data guru ini adalah sebuah website yang digunakan untuk memudahkan dalam mengelola data guru. Project ini saya buat pada semester 3 di Politeknik Negeri Cilacap.",
+    images: [
+      "assets/img/portfolio/dashboard-3.png",
+      "assets/img/portfolio/tambah-guru.png",
+      "assets/img/portfolio/edit-guru.png"
+    ]
+  }
+};
